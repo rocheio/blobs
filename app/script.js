6e780999d491e349ebdfa8a39b31890681b97dc8
Andy Roche <andy@roche.io>
2016-10-12 22:50:18 -0400

    add Camera class for infinite game scrolling

--- app/script.js
+++ app/script.js
@@ -4,20 +4,21 @@ Blobs: Game where everything is a blob.
 const CANVAS = document.getElementById('canvas');
 const CONTEXT = CANVAS.getContext('2d');
 const KEYCODEMAP = {
-    37: 'left', 38: 'up', 39: 'right', 40: 'down', 27: 'escape', 32: 'space'
+    37: 'left', 38: 'up', 39: 'right', 40: 'down',  // arrow keys
+    65: 'left', 87: 'up', 68: 'right', 83: 'down',  // w a s d
+    27: 'escape', 32: 'space'
 }
 
-// Resize the canvas, then redraw it
+// Fiddly bits to make the game more fun?
+const MAX_BLOBS = 30;
+const GRAPHICS_FPS = 30;
+const PHYSICS_FPS = 20;
+
+
+// Resize the canvas, then redraw it with the new size
 function resize_canvas () {
     CANVAS.width = window.innerWidth;
     CANVAS.height = window.innerHeight;
-    redraw_canvas();
-}
-
-// Redraw the canvas on the screen
-function redraw_canvas () {
-    CONTEXT.strokeStyle = 'blue';
-    CONTEXT.lineWidth = '5';
     CONTEXT.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 }
 
@@ -71,17 +72,25 @@ function step_toward(x1, y1, x2, y2, distance) {
 Game class that controls everything else
 */
 var Game = function () {
+    this.new_game();
+}
+// Start the game on object init and on restarts
+Game.prototype.new_game = function () {
     // Attributes
     this.score = 0;
-    this.max_blobs = 30;
     this.blobs = [];
     this.paused = false;
 
-    // Composition (big core chunks)
+    // Composition
     this.timer = new Timer();
     this.controls = new Controls(this);
-    this.graphics = new TrackedInterval(30, this.render_frame.bind(this));
-    this.physics = new TrackedInterval(10, this.render_action.bind(this));
+    this.camera = new Camera(Math.min(CANVAS.width, CANVAS.height) / 3);
+    this.graphics = new TrackedInterval(
+        GRAPHICS_FPS, this.render_frame.bind(this)
+    );
+    this.physics = new TrackedInterval(
+        PHYSICS_FPS, this.tick_physics.bind(this)
+    );
     this.spawner = new TrackedInterval(1, this.spawn_blob.bind(this));
     this.player = new Blob(CANVAS.width / 2, CANVAS.height / 2,
                            10, '#DDDDDD');
@@ -92,9 +101,9 @@ Game.prototype.render_frame = function () {
     CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
     // Draw Game stuff
     this.blobs.forEach(function(blob){
-        blob.draw();
-    });
-    this.player.draw();
+        blob.draw(this.camera.xoffset, this.camera.yoffset);
+    }.bind(this));
+    this.player.draw(this.camera.xoffset, this.camera.yoffset);
     this._draw_debug_info();
 };
 // Write debugging info to the canvas
@@ -117,19 +126,22 @@ Game.prototype._draw_debug_info = function () {
         CONTEXT.fillText(label_value, 5, ypos);
     });
 }
-// Spawn blobs on the canvas
+// Spawn blobs on the canvas near player but not within 50 px
 Game.prototype.spawn_blob = function () {
-    let xloc = rand_between(0, CANVAS.width);
-    let yloc = rand_between(0, CANVAS.height);
+    let buffer = 500;
+    let xloc = rand_between(this.player.xloc - buffer,
+                            this.player.xloc + buffer);
+    let yloc = rand_between(this.player.yloc - buffer,
+                            this.player.yloc + buffer);
     let radius = rand_between(this.player.radius * 0.25,
-                              this.player.radius * 1.5);
-    if (this.blobs.length < this.max_blobs) {
+                              this.player.radius * 1.25);
+    if (this.blobs.length < MAX_BLOBS) {
         this.blobs.push(new Blob(xloc, yloc, radius));
     }
 }
 // Taken action for every item on the canvas
 // Loop backward so removal from the list doesn't break things
-Game.prototype.render_action = function () {
+Game.prototype.tick_physics = function () {
     // Move NPC blobs
     for (let i = this.blobs.length - 1; i >= 0; i--) {
         let blob = this.blobs[i]
@@ -146,8 +158,11 @@ Game.prototype.render_action = function () {
         }
     }
     // Move the player
-    this.player.move(this.controls.intent_x,
-                     this.controls.intent_y);
+    let move_x = this.controls.intent_x;
+    let move_y = this.controls.intent_y;
+    this.player.move(move_x, move_y);
+    // Update the camera view if player is at boundaries
+    this.camera.adjust(this.player.xloc, this.player.yloc, move_x, move_y);
 }
 // Stop all action in the game world until Game is unpaused
 Game.prototype.toggle_pause = function () {
@@ -170,13 +185,9 @@ Game.prototype.game_over = function () {
     CONTEXT.fillText('score: ' + this.score, display_x, display_y + 25);
 }
 // Reset the game
-Game.prototype.reset = function () {
+Game.prototype.restart = function () {
     this.pause();
-    this.blobs = [];
-    this.timer.reset();
-    this.player = new Blob(CANVAS.width / 2, CANVAS.height / 2,
-                           10, '#DDDDDD');
-    this.start();
+    this.new_game();
 }
 // Start the game
 Game.prototype.start = function () {
@@ -193,6 +204,28 @@ Game.prototype.pause = function () {
     this.spawner.pause();
     this.timer.pause();
     this.paused = true;
+}
+
+
+/*
+Represents a Camera to view all Game objects.
+The view will only display a part of the game world.
+*/
+var Camera = function (boundary) {
+    this.xoffset = 0;
+    this.yoffset = 0;
+    this.boundary = boundary;
+}
+// Adjust the offset based on player location and movement speed
+Camera.prototype.adjust = function (xloc, yloc, move_x, move_y) {
+    if ((xloc > CANVAS.width - this.boundary) |
+            (xloc < this.boundary)) {
+        this.xoffset -= move_x;
+    }
+    if ((yloc > CANVAS.height - this.boundary) |
+            (yloc < this.boundary)) {
+        this.yoffset -= move_y;
+    }
 }
 
 
@@ -294,7 +327,7 @@ Controls.prototype.add_keyboard_listeners = function () {
         } else if (code == 'escape') {
             this.game.toggle_pause();
         } else if (code == 'space') {
-            this.game.reset();
+            this.game.restart();
         } else {
             this.direction(code);
         }
@@ -346,9 +379,10 @@ Blob.prototype.toString = function () {
             ') radius ' + this.radius + '>');
 }
 // Draw the blob on the canvas
-Blob.prototype.draw = function () {
+Blob.prototype.draw = function (xoffset=0, yoffset=0) {
     CONTEXT.beginPath();
-    CONTEXT.arc(this.xloc, this.yloc, this.radius, 0, 2 * Math.PI, false);
+    CONTEXT.arc(this.xloc + xoffset, this.yloc + yoffset,
+                this.radius, 0, 2 * Math.PI, false);
     CONTEXT.fillStyle = this.color;
     CONTEXT.fill();
     CONTEXT.lineWidth = 5;
